955e6231a85d9ac75095528aa7b8e93e91dbb9f1
mavrogiorgos <kostismvg@gmail.com>
2021-12-20 11:49:48 +0200

    Make prediction based on Naive Bayes algorithm.

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ from flask_cors import CORS, cross_origin
 import json
 import csv
 import random
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt       # matplotlib.pyplot plots data
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB # I am using Gaussian algorithm from Naive Bayes
 
 # Connect to our local MongoDB
 mongodb_hostname = os.environ.get("MONGO_HOSTNAME","localhost")
@@ -106,6 +113,58 @@ def sendemail():
    msg.body = "Hello Flask message sent from Flask-Mail"
    mail.send(msg)
    return "Sent"
+
+
+#this is the endpoint for predicting if a patient will need insulin or not
+@app.route('/predict',methods=['GET', 'POST'])
+@cross_origin()
+def predict():
+    if request.method == 'GET':
+        #get the needed arguments (Glucose,BloodPressure,Insulin,BMI,Age)
+        Glucose = request.args.get('Glucose')
+        BloodPressure = request.args.get('BloodPressure')
+        Insulin = request.args.get('Insulin')
+        BMI = request.args.get('BMI')
+        Age = request.args.get('Age')
+        #read the dataset that we will use to make the prediction
+        pdata = pd.read_csv("diabetes.csv")
+        columns = list(pdata)[0:-1] # Excluding Outcome column which is the one we want to predict 
+        n_true = len(pdata.loc[pdata['Outcome'] == True])
+        n_false = len(pdata.loc[pdata['Outcome'] == False])
+        #use only the specific columns to train our model
+        features_cols = ['Glucose', 'BloodPressure','Insulin', 'BMI', 'Age']
+        predicted_class = ['Outcome']
+        X = pdata[features_cols].values      # Predictor feature columns (5 X m)
+        Y = pdata[predicted_class]. values   # Predicted class (1=True, 0=False) (1 X m)
+        split_test_size = 0.30
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=split_test_size, random_state=52)
+        #Replace 0s with serial mean 
+        rep_0 = SimpleImputer(missing_values=0, strategy="mean")
+        x_train = rep_0.fit_transform(x_train)
+        x_test = rep_0.fit_transform(x_test)
+        #Train Naive Bayes algorithm 
+        # Lets creat the model
+        diab_model = GaussianNB()
+        diab_model.fit(x_train, y_train.ravel())
+        #Performance of our model with training data
+        diab_train_predict = diab_model.predict(x_train)
+        #Performance of our model with testing data
+        diab_test_predict = diab_model.predict(x_test)
+        # define input (the one that we took from the endpoint parameters)
+        new_input = [[Glucose,BloodPressure,Insulin,BMI,Age]]
+        #get prediction for new input
+        new_output = diab_model.predict(new_input)
+        #transform the array of new_output to string and check the value.
+        prediction_result = np.array2string(new_output)
+        #if it is 0 then return a message that says that the patient does not need insulin. If it is 1 then 
+        #suggest that the patient needs insulin
+        if "0" in prediction_result:
+           return Response('{"message":"Based on Naive Bayes ML algorithm, the patient does not need insulin."}', status=200, mimetype="application/json") 
+        elif "1" in prediction_result:
+            return Response('{"message":"Based on Naive Bayes ML algorithm, the patient needs insulin."}', status=200, mimetype="application/json")
+        else:
+            return Response('{"message":"Please try again."}', status=500, mimetype="application/json")
+        
 
 
 
